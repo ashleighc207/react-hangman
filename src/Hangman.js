04e3105d6219768e0083e87eaa88e2041b7282fd
Ashleigh Christopher <ashleighc207@gmail.com>
2019-09-18 13:44:28 -0400

    get hangman svg body setup

--- src/Hangman.js
+++ src/Hangman.js
@@ -4,7 +4,16 @@ import './Hangman.css'
 class Hangman extends Component {
   render(){
     return(
-      <div> Hangman </div>
+      <div className="Hangman">
+        <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="45" cy="20" r="10" fill="#FFFFFF" stroke="#212121" />
+          <line x1="45" y1="30" x2="45" y2="80" stroke="#212121" />
+          <line x1="45" y1="45" x2="60" y2="65" stroke="#212121" />
+          <line x1="45" y1="45" x2="30" y2="65" stroke="#212121" />
+          <line x1="45" y1="80" x2="55" y2="100" stroke="#212121" />
+          <line x1="45" y1="80" x2="35" y2="100" stroke="#212121" />
+        </svg>
+      </div>
     )
   }
 }
