04a367ba4e7d0512da96358bf3c7323fb4fda923
Ashleigh Christopher <ashleighc207@gmail.com>
2019-09-24 15:26:16 -0400

    Updates

---
+++ precache-manifest.cea219b865eecc305911b344ae148313.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6460b4f857e9ff76ec77fdc664717c6",
+    "url": "/react-hangman/index.html"
+  },
+  {
+    "revision": "a185f4e6f7427734765c",
+    "url": "/react-hangman/static/css/main.3d198ffb.chunk.css"
+  },
+  {
+    "revision": "f608958d65cc52dcd520",
+    "url": "/react-hangman/static/js/2.198e5109.chunk.js"
+  },
+  {
+    "revision": "a185f4e6f7427734765c",
+    "url": "/react-hangman/static/js/main.208308a1.chunk.js"
+  },
+  {
+    "revision": "61be5b489f9e60898714",
+    "url": "/react-hangman/static/js/runtime~main.26de8e6d.js"
+  }
+]);
